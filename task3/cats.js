40f399fd4183cd1ca8c84cc85e5f7a5e916c4584
Elena Sinelnikova <elle.sinelle@gmail.com>
2024-01-21 19:11:16 +0300

    feat: add unresolved task 3

---
+++ task3/cats.js
@@ -0,0 +1,15 @@
+const cats = [
+  {
+    src: 'assets/cat-1.png',
+  },
+  {
+    src: 'assets/cat-2.png',
+  },
+  {
+    src: 'assets/cat-3.png',
+  },
+  {
+    src: 'assets/cat-4.png',
+  }
+]
+export default cats;
---
+++ task3/script.js
@@ -0,0 +1,29 @@
+'use strict'
+import cats from '/cats.js';
+
+const btnPrev = document.querySelector('.btn-prev');
+const btnNext = document.querySelector('.btn-next');
+const imgContainer = document.querySelector('.image-container');
+
+window.addEventListener('load', function() {
+  imgContainer.style.backgroundImage = `url(${cats[0].src})`;
+})
+
+function showNext(i) {
+  i = cats[i];
+  imgContainer.style.backgroundImage = '';
+  imgContainer.style.backgroundImage = `url(${cats[i+1].src})`;
+  i = cats[i+1];
+  if (i == 3) i = 0;
+}
+
+function showPrev(i) {
+  i = cats[0];
+  imgContainer.style.backgroundImage = '';
+  imgContainer.style.backgroundImage = `url(${cats[i-1].src})`;
+  i = cats[i-1];
+  if (i == -1) i = 3;
+ }
+
+btnNext.addEventListener('click', showNext);
+btnPrev.addEventListener('click', showPrev);
