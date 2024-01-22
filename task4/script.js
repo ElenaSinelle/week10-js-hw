6bae06fbc440fea188f0eec34ef5de9c7c706c21
Elena Sinelnikova <elle.sinelle@gmail.com>
2024-01-22 14:11:05 +0300

    fix: fix script in task 4

--- task4/script.js
+++ task4/script.js
@@ -1,17 +1,14 @@
 'use strict'
 const calcBtn = document.querySelector('.calc-btn');
 
-let item1Qty = document.querySelector('.item-1-qty');
-let item2Qty = document.querySelector('.item-2-qty');
-
 const item1Price = document.querySelector('.item-1-price').textContent;
 const item2Price = document.querySelector('.item-2-price').textContent;
 
 const messageContainer = document.querySelector('.total-price-calculated');
 
 calcBtn.addEventListener('click', function(){
-  item1Qty = item1Qty.value;
-  item2Qty = item2Qty.value;
+  let item1Qty = document.querySelector('.item-1-qty').value;
+  let item2Qty = document.querySelector('.item-2-qty').value;
 
   messageContainer.textContent = calculateTotalPrice([item1Qty, item1Price], [item2Qty, item2Price]);
 })
