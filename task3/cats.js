c13bda80cc75723ebbbc92a2fff46fe06c73581e
Elena Sinelnikova <elle.sinelle@gmail.com>
2024-01-22 09:41:14 +0300

    fix: change cats images

--- task3/cats.js
+++ task3/cats.js
@@ -1,15 +1,15 @@
 const cats = [
   {
-    src: 'assets/cat-1.png',
+    src: 'assets/cat-1.jpg',
   },
   {
-    src: 'assets/cat-2.png',
+    src: 'assets/cat-2.jpg',
   },
   {
-    src: 'assets/cat-3.png',
+    src: 'assets/cat-3.jpg',
   },
   {
-    src: 'assets/cat-4.png',
+    src: 'assets/cat-4.jpg',
   }
 ]
 export default cats;
